426195c9e781e816a89540387327d92ba7b655ee
asewnath <akira.sewnath@gmail.com>
2024-05-14 10:15:21 -0400

    update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -7,6 +7,7 @@ import sys
 ###################################################
 # edit things below as appropriate for your project
 sys.path.insert(0, os.path.abspath('eva/src/'))
+sys.path.insert(0, '/home/runner/work/eva/eva/eva'))
 
 project = u''
 copyright = u''
