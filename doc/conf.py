44ccb764e7176a7030e2a38e7ae62cf7f5ee6d6a
asewnath <akira.sewnath@gmail.com>
2024-05-14 11:13:32 -0400

    conf update

--- doc/conf.py
+++ doc/conf.py
@@ -9,6 +9,7 @@ import sys
 sys.path.insert(0, os.path.abspath('eva/src/'))
 sys.path.insert(0, '/home/runner/work/eva/eva/eva'))
 sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('./eva/src/'))
 
 project = u''
 copyright = u''
